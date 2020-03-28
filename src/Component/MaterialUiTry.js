9888e23e00c274bb59fb3747a1d63990563fe3c1
unknown <18103052@iubat.edu>
2020-03-29 02:04:24 +0600

    keep parcticing on react redux

---
+++ src/Component/MaterialUiTry.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+
+const MaterialUiTry = () => {
+    return (
+        <div>
+        <Button variant="contained" color="primary">
+        Hello World
+        </Button>
+        </div>
+    );
+};
+
+export default MaterialUiTry;
